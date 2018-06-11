162e84381646edc774286776a593177c1bb8901d
Andrew Plaza <aplaza@liquidthink.net>
2018-06-11 16:16:10 -0400

    added proj comp

---
+++ src/pages/projects.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import _ from 'lodash';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+
+
+
