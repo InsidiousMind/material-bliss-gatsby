cc8b39674dd04354bef0ab7ad45d84f25e7f75c2
Andrew Plaza <aplaza@liquidthink.net>
2018-06-11 14:14:26 -0400

    add projects

---
+++ projects.js
@@ -0,0 +1,232 @@
+// all my projects for the /projects/ part of the website
+
+module.exports = [
+  {
+    homepage: "http://scrantonhacks.com",
+    title: "ScrantonHacks",
+    cardImage: "static/img/scrantonhacks.png",
+    cardTitle: "ScrantonHacks",
+    subtitle: "Website for Hackathon Club at Scranton",
+    date: "Sept 2017",
+    tags: [ 'University', 'Club','Hackathon', 'React','Javascript'],
+    description: "A website built with React and Gatsby for the University of Scranton Hackathon club.",
+  },
+  {
+    homepage: "http://www.intsam.org",
+    title: "International Samaritan",
+    cardImage: "static/img/intsam.png",
+    subtitle: "Built a website, freelance, for a non-profit",
+    cardTitle: "International Samaritan",
+    date: "2013-2017",
+    tags: [
+      "blog",
+      "personal",
+      "general",
+      "Rantings",
+      "nerdy"
+    ],
+    description: "International Samaritan is a Non-Profit NGO which serves the most impoverished peoples of the world. They needed a website refresh, so I stepped up to the occasion."
+  },
+  {
+    homepage: "https://github.com/ethdbg/ethdbg",
+    title: "Ethereum Debug Tools",
+    cardImage: "static/img/ethereum.png",
+    cardTitle: "ETHDBG",
+    date: "Jan 2018-Present",
+    tags: [
+      "project",
+      "NodeJS",
+      "Ethereum",
+      "Solidity",
+      "Viper",
+      "Debugging",
+      "VSCode"
+    ],
+    description: "Debug Tools to be used with the Ethereum Blockchain and help developers create Decentralized Applications."
+  },
+  {
+    homepage: "https://github.com/ScrantonHacks/YADQL",
+    cardImage: "static/img/yadql.png",
+    cardTitle: "YADQL",
+    title: "YADQL (Prototype)",
+    date: "Jan 2018-March 2018",
+    tags: [
+      "project",
+      "Database",
+      "Query Languages",
+      "Blockchain"
+    ],
+    description: "YADQL - Yet Another Database Query Language is a tool with which to use decentralized technologies such as Ethereum or IPFS, and treat it just like a more familiar Database system such as SQL."
+  },
+  {
+    homepage: "http://emotifuck.rs",
+    cardImage: "static/img/brainfuck.png",
+    cardTitle: "Emotif*ck",
+    date: "October 2017",
+    tags: [
+      "project",
+      "Rust",
+      "Esoteric",
+      "Fun"
+    ],
+    description: "An esoteric programming language based off of brainfuck"
+  },
+  {
+    homepage: "http://www.github.com/InsidiousMind/code.LiquidThink",
+    cardImage: "static/img/code-liquidthink.png",
+    cardTitle: "Code.Liquid_Think",
+    cardSubtitle: "this site!",
+    date: "Jan 2017-Present",
+    tags: [
+      "project",
+      "React",
+      "Javascript",
+      "Jekyll"
+    ],
+    description: "This blog where I post on just programming. Anything that i'm really doing, or am planning to do, is going on this site. The source code and theme of this site is my Material Bliss Theme"
+  },
+  {
+    title: "Material Bliss theme",
+    subtitle: "Theme for Jekyll built with React.js",
+    homepage: "https://github.com/InsidiousMind/material-bliss-jekyll-theme",
+    cardImage: "static/img/logo_theme.jpg",
+    date: "Dec 2016-Present",
+    tags: [
+      "Javascript",
+      "React",
+      "CSS/HTML",
+      "Jekyll",
+      "Ruby"
+    ],
+    description: "The theme of this site that is under continual development.",
+    author: "Andrew Plaza",
+    author_tag: "",
+    avatar: true
+  },
+  {
+    cardImage: "static/img/gomoku.svg.png",
+    cardTitle: "Gomoku",
+    subtitle: "A Networked Client+Server Retro Game",
+    homepage: "http://www.github.com/InsidiousMind/Gomoku",
+    date: "Nov 2016",
+    tags: [
+      "C",
+      "Python",
+      "Curses",
+      "CLI",
+      "Retro Game"
+    ],
+    description: "A Networked Gomoku Game. Includes a Server backend in C and a frontend built with Python and curses CLI. Future plans include rewriting the server with Libuv/Node",
+    author: "Andrew Plaza, Sean Batzel",
+    author_tag: "Partner Project",
+    avatar: true
+  },
+  {
+    title: "Contributing to libuv",
+    subtitle: "Cross-platform asynchronous I/O",
+    homepage: "https://github.com/libuv/libuv",
+    cardImage: "static/img/libuv.png",
+    date: "Dec 2016-Present",
+    tags: [
+      "C",
+      "Node.js",
+      "library",
+      "open-source"
+    ],
+    description: "In my effort to increase my contribution to Open Source Projects I chose to contribute to libuv, the library developed for Node.js. This facilitates my interest in Node.js and how it works on a deeper level, whilst also allowing me to improve upon my C programming skills and give back to the OSS community."
+  },
+  {
+    title: "Contributing to Redox OS",
+    subtitle: "Your Next[Gen] OS",
+    homepage: "https://github.com/redox-os/kernel",
+    cardImage: "static/img/redox.png",
+    date: "March 2017-Present",
+    tags: [
+      "Rust",
+      "OS",
+      "open-source"
+    ],
+    description: "An OS I have been faithfully following since it's inception 2 years ago. Equipped with what I learned from an OS class and my own learning ventures, I began contributing and conversing actively with the community in March 2017"
+  },
+  {
+    author: "Andrew Plaza",
+    author_tag: "Creative Initiative",
+    title: "SMPD",
+    subtitle: "Simple Music Player Daemon",
+    avatar: true,
+    homepage: "https://github.com/InsidiousMind/smpd",
+    date: "April, 2017",
+    tags: [
+      "Rust",
+      "Open-source",
+      "MusicPlayerDaemon"
+    ],
+    description: "An Open Source Project being designed as a drop-in replacement for MPD except in Rust. The first thing to be completed for this is SNES_SPC emulation. Why? Because SNES music is fun."
+  },
+  {
+    author: "Andrew Plaza",
+    author_tag: "Creative Initiative (Concept)",
+    avatar: true,
+    title: "Revolution",
+    homepage: "https://github.com/RevolutionApp/Revolution",
+    cardImage: "static/img/revolution_logo.png",
+    date: "TBA",
+    tags: [
+      "Javascript",
+      "Ethereum",
+      "Prototype",
+      "Concept",
+      "react-native"
+    ],
+    description: "A concept for a decentralized mobile Application built with React Native. The project is in it's design/concept/protyping phase. This will be an application to connect with local protests, communicate with activists and organizations in a totally anonymous, encrypted manner."
+  },
+  {
+    author: "Andrew Plaza, Joe LoManto, Ryan Corcoran",
+    author_tag: "Hackathon Group Project",
+    avatar: true,
+    homepage: "https://github.com/YourNerdyJoe/leapduel",
+    cardImage: "static/img/leapduel.jpg",
+    cardTitle: "LeapDuel",
+    cardSubtitle: "Cardgame built with LeapMotion at hackPSU2016",
+    date: "Nov 12, 2016",
+    tags: [
+      "C++",
+      "LeapMotion",
+      "Card Game",
+      "Yugioh",
+      "HackPSU"
+    ],
+    description: "An awesome throw-back to Yugioh using the LeapMotion Controller, a modern twist on a retro piece of nostalgia we remembered from our childhoods."
+  },
+  {
+    title: "Liquid Think",
+    subtitle: "Rantings of a Nerdy Lunatic",
+    homepage: "http://www.liquidthink.net",
+    cardImage: "static/img/LQ_logo.png",
+    date: "2013-2017",
+    tags: [
+      "blog",
+      "personal",
+      "general",
+      "Rantings",
+      "nerdy"
+    ],
+    description: "A blog where I talk about everything from Lovecraft to new technology and programming. Not anything too technical here, that goes on code.liquid_think"
+  },
+  {
+    title: "Party Fair Chester",
+    subtitle: "Local Business Landing Page",
+    homepage: "http://partyfairchester.com",
+    cardImage: "src/uploads/2017/02/04/partyfairchester.png",
+    date: "2016-08-01T00:00:00-04:00",
+    tags: [
+
+    ],
+    description: "Landing Page developed for the LucheLLC. Designed in a poppy, fun, and unique manner serves as a reflection of the stores identity.",
+    cardTitle: "",
+    cardSubtitle: "",
+    author: "Andrew Plaza",
+    author_tag: "Web Developer",
+      avatar: true
+    }
+];
